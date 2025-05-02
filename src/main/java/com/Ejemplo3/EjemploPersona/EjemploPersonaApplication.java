b27c37d19d80192dcd7b7ad776195dffebfc1c20
Felipe Inzunza <feli.inzunza@duocuc.cl>
2025-05-02 00:15:43 -0400

    primercomit

---
+++ src/main/java/com/Ejemplo3/EjemploPersona/EjemploPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.Ejemplo3.EjemploPersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ejemplo3/EjemploPersona/EjemploPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejemplo3.EjemploPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
