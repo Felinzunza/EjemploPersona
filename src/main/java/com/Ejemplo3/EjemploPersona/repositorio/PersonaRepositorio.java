eb6659768ea9b6ff84440fae84dd777a3ba44b4c
Felipe Inzunza <feli.inzunza@duocuc.cl>
2025-05-05 23:50:51 -0400

    cambio repositorio

--- src/main/java/com/Ejemplo3/EjemploPersona/repositorio/PersonaRepositorio.java
+++ src/main/java/com/Ejemplo3/EjemploPersona/repositorio/PersonaRepositorio.java
@@ -17,6 +17,39 @@ public class PersonaRepositorio {
             return persona;
         }
 
+        public List<Persona> readAll(){ /*Devuelve todas las personas de la lista*/
+            return personas; /*Devuelve la lista de todas las personas*/
+        }
 
-} // Luego se usa en el servicio para definir cómo se accede a estos datos
+        public Persona read (int id){ /*Busca una persona por id*/
+            for (Persona persona : personas) { /*Recorre la lista de personas*/
+                if (persona.getId() == id) { /*Si el id coincide con el que se busca*/
+                    return persona; /*Devuelve la persona*/
+                }
+                     
+        }
+            return null; /*Si no se encuentra la persona devuelve null*/
+        }
+
+        public Persona update (int id, Persona nuevaPersona){ /*Actualiza una persona por id*/
+            Persona personaEncontrada = read(id);  /*Busca la persona por id*/
+            if (personaEncontrada != null) { /*Si la persona existe*/
+                personaEncontrada.setNombre(nuevaPersona.getNombre()); /*Actualiza el nombre*/
+                personaEncontrada.setApellido(nuevaPersona.getApellido()); /*Actualiza el apellido*/
+                personaEncontrada.setEdad(nuevaPersona.getEdad()); /*Actualiza la edad*/
+                return personaEncontrada; /*Devuelve la persona actualizada*/
+            }
+            return null;
+         } /*Si no se encuentra la persona devuelve null*/
+
+         public String delete (int id){ /*Elimina una persona por id*/
+            Persona personaEncontrada = read(id); /*Busca la persona por id*/
+            if (personaEncontrada != null) { /*Si la persona existe*/
+                personas.remove(personaEncontrada); /*Elimina la persona de la lista*/
+                return "Persona eliminada"; /*Devuelve String de confirmacion*/
+            } 
+            return "Persona no encontrada"; /*Si no se encuentra la persona devuelve String de error*/}
+            
+} 
+ // Luego se usa en el servicio para definir cómo se accede a estos datos
 
